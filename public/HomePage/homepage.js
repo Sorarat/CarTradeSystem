c515cd2e17f631053293271499cf4f6475f8cbc4
XiaoJuan-C <xiaojuan1727@gmail.com>
2024-10-21 19:06:44 +0800

    Modification
    Homepage
    - header (standard)
    - update CSS & JS
    
    Admin - rename files

--- public/HomePage/homepage.js
+++ public/HomePage/homepage.js
@@ -1,4 +1,26 @@
+/* Header Javascript */
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function menuBtn() {
+    document.getElementById("menu-dropdown").classList.toggle("show");
+}
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("menu-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
 function logoutBtn(event){
     event.preventDefault(); // Prevent the form from submitting
     document.location.href = "../LogoutPage/LogoutPage.html"; // Use relative path (one directory level up)
 }
+/* ---------------------------------- */
