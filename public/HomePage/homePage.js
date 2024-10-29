24081aea9d1672be8bec02ba351a74f5fe21907e
XiaoJuan-C <xiaojuan1727@gmail.com>
2024-10-29 16:31:32 +0800

    homePage update
    homePage
    carDetailsPage

--- public/HomePage/homePage.js
+++ public/HomePage/homePage.js
@@ -2,25 +2,62 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function menuBtn() {
-    document.getElementById("menu-dropdown").classList.toggle("show");
+  document.getElementById("menu-dropdown").classList.toggle("show");
 }
-  
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("menu-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("menu-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
+  }
 }
 
 function logoutBtn(event){
-    event.preventDefault(); // Prevent the form from submitting
-    document.location.href = "../LogoutPage/LogoutPage.html"; // Use relative path (one directory level up)
+  event.preventDefault(); // Prevent the form from submitting
+  document.location.href = "../logoutPage/logoutPage.html"; // Use relative path (one directory level up)
 }
+
+/* ---------------------------------- */
+/* homePage JS */
+/* Search button */
+function performCarSearch() {
+/* TO DO... */
+}
+
+/* View Details button */
+function viewCarDetails() {
+window.location.href = "./carDetailsPage.html"; // Redirect to the specified page
+}
+
+/* ---------------------------------- */
+/* carDetailsPage JS */
+
+/* Loan Calculator - TO DO... */
+
+// JS code to convert string/int fetched from db to SGD format
+// Define the price you want to format
+const price = 79000;
+
+// Create a formatter for Singapore Dollars
+const sgdFormatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 0, // No decimal places
+  maximumFractionDigits: 0, // No decimal places
+});
+
+// Format the price
+const formattedPrice = `$${formatter.format(price)}`;
+
+// Output the formatted price
+console.log(formattedPrice); // Output: $79,000
+
+/* ---------------------------------- */
+/* viewRatingReviewPage JS */
+
 /* ---------------------------------- */
